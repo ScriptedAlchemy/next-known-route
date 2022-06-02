a70ae2ae183c0e74948b9f9c88c83e1d772b1149
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-06-02 15:14:46 -0700

    support outside of webpack

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const NextRouter = require("next/router");
-const {getRouteRegex, getRouteMatcher} = require('next/dist/shared/lib/router/utils')
-
+const { getRouteRegex, getRouteMatcher } = require("next/dist/shared/lib/router/utils");
 let knownRoutes = [];
-if(typeof window !== "undefined") {
+if (typeof window !== "undefined") {
     try {
         knownRoutes = window.__NEXT_DATA__.props.knownRoutes || window.__NEXT_DATA__.props.layoutProps.knownRoutes;
-    } catch(e) {
+    } catch (e) {
 
     }
     NextRouter.ready(function() {
         try {
-            NextRouter.router.pageLoader.getPageList().then(function (pageList) {
-                pageList.forEach(function (page) {
+            NextRouter.router.pageLoader.getPageList().then(function(pageList) {
+                pageList.forEach(function(page) {
                     if (!knownRoutes.includes(page)) {
                         knownRoutes.push(page);
                     }
-                })
-            })
+                });
+            });
         } catch (e) {
-            console.error(e)
+            console.error(e);
         }
     });
 }
@@ -28,33 +27,47 @@ if(typeof window !== "undefined") {
  * @returns {*[]}
  */
 module.exports.getRouteManifest = function getRouteManifest(additionalRoutes = []) {
-    if(typeof window !== "undefined") {
-        additionalRoutes.forEach(function (page) {
+    if (typeof window !== "undefined") {
+        additionalRoutes.forEach(function(page) {
             if (!knownRoutes.includes(page)) {
                 knownRoutes.push(page);
             }
         });
     } else {
-        const path = require('path')
-        let requireFunc = __non_webpack_require__ || require
-        let serverRoutes
+        const path = require("path");
+        let requireFunc;
+        if (typeof __non_webpack_require__ === "undefined") {
+            requireFunc = require;
+        } else {
+            requireFunc = __non_webpack_require__;
+        }
+
+        let serverRoutes = [];
+        let pageManifest;
+        if (fs.existsSync(path.join(__dirname, "pages-manifest.json"))) {
+            pageManifest = path.join(__dirname, "pages-manifest.json");
+        } else if (fs.existsSync(path.join(__dirname, "../pages-manifest.json"))) {
+            pageManifest = path.join(__dirname, "../pages-manifest.json");
+        } else if (fs.existsSync(path.join(process.cwd(), ".next", "pages-manifest.json"))) {
+            pageManifest = path.join(process.cwd(), ".next", "pages-manifest.json");
+        }
         try {
-            serverRoutes = Object.keys(requireFunc(path.join(__dirname, 'pages-manifest.json')))
+            serverRoutes = Object.keys(requireFunc(pageManifest));
         } catch (e) {
-            serverRoutes = Object.keys(requireFunc(path.join(__dirname, '../pages-manifest.json')))
+            console.error(e);
         }
         knownRoutes = Array.from(new Set([].concat(serverRoutes, additionalRoutes)));
     }
 
-    return knownRoutes
-}
+    return knownRoutes;
+};
 /**
  * Checks URL against known page routes
  * @param url
  * @returns {boolean}
  */
 module.exports.isKnownRoute = function isKnownRoute(url) {
-    return knownRoutes.some(function (routeExp) {
+    return knownRoutes.some(function(routeExp) {
         var routeRegex = getRouteRegex(routeExp);
         var routeMatcher = getRouteMatcher(routeRegex);
         return routeMatcher(url);
